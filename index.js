d197b44334934a51a5b91156e2eb81f41dbf9c83
Mubin987 <99292441+Mubin987@users.noreply.github.com>
2023-12-13 09:48:26 +0500

    first commit

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ const cors = require('cors');  //Cross-origin resource sharing
 require('dotenv').config();
 
 app.use(express.json());
-app.use(cors({
-    origin: 'https://talent-globe.netlify.app', // Adjusted with frontend URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Enable CORS credentials (cookies, HTTP authentication)
-  }));
+app.use(cors({}));
 
 const db = require('./models');
 
